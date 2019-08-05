f21c7b507e65e0cc84c036878bb98784d5593be7
Eporon <errick.poron@gmail.com>
2019-08-05 01:55:56 -0600

    HT4

--- CircularLinkedList.py
+++ CircularLinkedList.py
@@ -1,3 +1,6 @@
+import os
+rutadeubicacion = os.path.dirname(os.path.abspath(__file__))
+
 class node:
     def __init__(self,name=None,score=None,next=None,prior =None):
         self.name =name
@@ -9,7 +12,20 @@ class CircularLinkedList:
     def __init__(self):
         self.first = None
         self.last = None
+        self.item = None
     
+    def nextitem(self):
+        if self.item ==None:
+            self.item = self.first
+            return self.item.name
+        else:
+            self.item = self.item.next
+            return self.item.name
+    def prioritem(self):
+        if self.item != None:
+            self.item = self.item.prior
+            return self.item.name
+
     def is_Empty(self):
         if self.first ==None:
             return True
@@ -84,10 +100,39 @@ class CircularLinkedList:
         else:
             self.last =self.last.prior
         self.linknode()
-     
+
+    def Report_User(self):
+            nodonum =0
+            f_output = open(rutadeubicacion+'\\Report_Users.txt','w')
+            f_output.write("digraph{\n node[shape = record,style=filled, color = lightgray];")
+            f_output.write("label = \"Circular Linked List of Users\"; \n")
+            f_output.write("\n")
+            
+            aux = self.first
+            while aux:
+                f_output.write("\t\t d%d[label=\"%s\"];\n"%(nodonum,aux.name))
+                aux = aux.next
+                nodonum+=1
+                if aux == self.first:
+                 break
+            f_output.write("{rank=same")
+            for i in range(0,(nodonum//2)+1):
+                 f_output.write("d%d; d%d;"%(i,nodonum-1-i))
+            f_output.write("}\n")
+
+            for i in range(0,nodonum):
+                if i ==nodonum-1:
+                     f_output.write("\t\t d%d->d%d[color =\"yellow1\"];\n"%(i,0))
+                     f_output.write("\t\t d%d->d%d[color =\"yellow1\"];\n"%(0,i))
+                else:
+                    f_output.write("\t\t d%d->d%d[color =\"yellow1\"];\n"%(i,i+1))
+                    f_output.write("\t\t d%d->d%d[color =\"yellow1\"];\n"%(i+1,i))    
+            f_output.write("\t }")        
+            f_output.close()
+            os.system("C:/Users/Graphviz2.38/bin/neato.exe -Tpng "+rutadeubicacion+"\\Report_Users.txt -o"+rutadeubicacion+"\\Report_Users.png" )
 
 
-
+'''
 lista = CircularLinkedList()
 
 
@@ -100,5 +145,4 @@ lista.add_last('u')
 lista.startnode()
 print("*"*25)
 lista.finishnode()
-
-        
+'''
